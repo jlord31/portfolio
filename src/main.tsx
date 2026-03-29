48f979e1705d4d734d62bcefae9f07d805941188
jlord31 <adepoju97@gmail.com>
2026-03-29 17:52:01 +0300

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import ThemeProvider from "./providers/theme-provider";
 import { routeTree } from "./routeTree.gen";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 const router = createRouter({ routeTree });
 
@@ -19,6 +20,7 @@ createRoot(document.getElementById("root")!).render(
     <ThemeProvider defaultTheme="dark" storageKey="portfolio-theme">
       <RouterProvider router={router} />
       <Toaster />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>,
 );
